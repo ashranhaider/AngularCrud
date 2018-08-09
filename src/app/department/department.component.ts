becd35e1ee1a50a929c918a00ccd16fb6b688e21
Ashran Haider <ashran.haider@fincon-services.com>
2018-08-09 17:41:52 +0500

    separate components for dep, emp and home and worked on structure

---
+++ src/app/department/department.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+@Component({
+  selector: 'app-department',
+  templateUrl: './department.component.html',
+  styleUrls: ['./department.component.css']
+})
+export class DepartmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  nameFormControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  locationFormControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  matcher = new MyErrorStateMatcher();
+}
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -2,14 +2,6 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 
-/** Error when invalid control is dirty, touched, or submitted. */
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
-
 /** @title Input with a custom ErrorStateMatcher */
 @Component({
   selector: 'app-employee',
@@ -17,18 +9,5 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./Employee.Component.css'],
 })
 export class EmployeeComponent {
-  nameFormControl = new FormControl('', [
-    Validators.required
-  ]);
-
-  locationFormControl = new FormControl('', [
-    Validators.required
-  ]);
-
-  matcher = new MyErrorStateMatcher();
+  
 }
-
-
-/**  Copyright 2018 Google Inc. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at http://angular.io/license */
